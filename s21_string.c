fa25de0c2afba4c1cb9adf69aaba5c81fd1660f0
d1ksan <ekhestt@yandex.ru>
2026-02-07 03:59:50 +0300

    feat: add memset-function

--- s21_string.c
+++ s21_string.c
@@ -21,3 +21,14 @@ char *s21_strncpy(char *dest, const char *src, s21_size_t n){
 
     return dest;
 }
+
+void *s21_memset(void *str, int c, s21_size_t n) {
+    unsigned char *ptr = (unsigned char *)str;
+    unsigned char value = (unsigned char)c;
+    
+    for (s21_size_t i = 0; i < n; i++) {
+        ptr[i] = value;
+    }
+    
+    return str;
+}
--- s21_string.h
+++ s21_string.h
@@ -8,5 +8,6 @@ typedef unsigned long s21_size_t;
 #define S21_NULL ((void*)0)
 s21_size_t s21_strlen(const char *str);
 char *s21_strncpy(char *dest, const char *src, s21_size_t n);
+void *s21_memset(void *str, int c, s21_size_t n);
 
 #endif 
