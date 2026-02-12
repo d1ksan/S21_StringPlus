2cad11a4e1749f600c962f32e0ee729dbf1f5274
d1ksan <ekhestt@yandex.ru>
2026-02-12 15:47:13 +0300

    feat: add memcmp-function

--- s21_string.c
+++ s21_string.c
@@ -54,3 +54,15 @@ void *s21_memchr(const void *str, int c, s21_size_t n) {
     }
     return S21_NULL;
 }
+
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n) {
+    const unsigned char *p1 = (const unsigned char *)str1;
+    const unsigned char *p2 = (const unsigned char *)str2;
+
+    for (s21_size_t i = 0; i < n; i++) {
+        if (p1[i] != p2[i]) {
+            return (int)(p1[i] - p2[i]);  // разница как int
+        }
+    }
+    return 0;
+}
--- s21_string.h
+++ s21_string.h
@@ -11,5 +11,6 @@ char *s21_strncpy(char *dest, const char *src, s21_size_t n);
 void *s21_memset(void *str, int c, s21_size_t n);
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
 void *s21_memchr(const void *str, int c, s21_size_t n);
+int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
 
 #endif 
